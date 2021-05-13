1bef180246633e97c665bd9d33f6cea4a2aa9498
Augusto Porres <augusporres@frba.utn.edu.ar>
2021-05-13 16:25:49 -0300

    Remplazo sintaxis listo

---
+++ ej2/replace_sintax.py
@@ -0,0 +1,9 @@
+import re
+
+with open('testcase.v', 'r') as f, open('my_expected.v', 'w') as d:
+    file_content = f.read()
+    new_content = re.sub(r'  reg \[(.*)\] (\S*) \[(.*)\];\n  initial begin\n((    \S*\[\S*\] = \S*;\n)*)  end\n', '  reg [7:0] mem [15:0];\n  $readmemh("memdump0.mem", mem);\n', file_content, flags=0)
+    d.write(new_content)
+
+
+
